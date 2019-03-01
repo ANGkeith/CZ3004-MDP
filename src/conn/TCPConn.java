c54d3acd622a8a7fb337cd33ba00a53be665fb79
lokwrd <darenlok@hotmail.com>
2019-03-01 09:31:07 +0800

    Added TCPConn class
    Added new TCPConn class and static strings for message reading with RPI

---
+++ src/conn/TCPConn.java
@@ -0,0 +1,64 @@
+package conn;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+import models.Constants;
+
+public class TCPConn {
+	
+	public static final String RPI_IP = "";
+	public static final int RPI_PORT = 1234;
+	
+	private static TCPConn mInstance;
+	private Socket mSocket;
+	private PrintWriter mWriterToRPI;
+	private Scanner mScannerFromRPI;
+	
+	private TCPConn() {}
+	
+	public static TCPConn getInstance() {
+		if (mInstance == null)
+			mInstance = new TCPConn();
+		return mInstance;
+	}
+	
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		TCPConn conn = TCPConn.getInstance();
+		conn.instantiateConnection(RPI_IP, RPI_PORT);
+		System.out.println("Connection Successful!");
+		while (true)
+		{
+			conn.sendMessage(Constants.READ_SENSOR_VALUES);
+			String msgReceived = conn.readMessage();
+			System.out.println("Message received: "+ msgReceived);
+		}
+	}
+	
+	public void instantiateConnection (String ip, int port) throws UnknownHostException, IOException {
+		mSocket = new Socket(RPI_IP, RPI_PORT);
+		mWriterToRPI = new PrintWriter(mSocket.getOutputStream());
+		mScannerFromRPI = new Scanner(mSocket.getInputStream());
+	}
+	
+	public void endConnection () throws IOException {
+		if (mSocket.isClosed() != true)
+			mSocket.close();
+	}
+	
+	public void sendMessage(String msg) throws IOException {
+		mWriterToRPI.print(msg);
+		mWriterToRPI.flush();
+		System.out.println("Message sent: " + msg);
+	}
+
+	public String readMessage() throws IOException {
+		String msgRecieved = mScannerFromRPI.nextLine();
+		System.out.println("Message received: " + msgRecieved);
+		return msgRecieved;
+}
+	
+}
--- src/models/Constants.java
+++ src/models/Constants.java
@@ -27,11 +27,25 @@ public class Constants {
     public static final int DEFAULT_COVERAGE_LIMIT = 100;
     public static final int DEFAULT_TIME_LIMIT = 120;
 
+    public static final String FORWARD = "W";
+    public static final String TURN_LEFT = "A";
+    public static final String TURN_RIGHT = "D";
+	public static final String EXPLORE_DONE = "F";
+	public static final String SEPARATOR = "|";
+	public static final String READ_SENSOR_VALUES = "E";
+	public static final String DONE = "Y";
+	public static final String START_EXPLORATION = "explore";
+	public static final String START_SHORTEST = "short";
+	public static final String CALIBRATE = "C";
+	public static final String CALIBRATE_PATTERN = ":1:[0-9]:1:[0-9]:[0-9][|]";
+    
+    
     public enum My_Robot_Instruction {
         FORWARD,
         TURN_LEFT,
         TURN_RIGHT,
-    };
+    }
+    
 
     public enum Orientation {
         N,
